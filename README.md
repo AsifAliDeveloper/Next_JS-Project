baf6462d23e7e35f9a83f6e2799e42f5d4466cb4
Asif Ali <128137458+AsifAliDeveloper@users.noreply.github.com>
2026-01-26 19:45:16 +0500

    Add Next-Dashboard

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Next_JS-Project
-This is my Next JS Repo for learning and make Projects
+# Date: 26-01-26
+
+## Getting Started
+
+# Next Dashboard
+
+## Learn More
+
+## Deploy on Vercel
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Welcome to My Next.js Dashboard</h1>
+      <p>Next.js Setup Completed</p>
+    </main>
+  );
+}
